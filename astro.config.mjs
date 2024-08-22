0c4cbcf148687f0a6afa4d22df00b81fb904473e
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-08-22 19:15:01 -0400

    more stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({output: "server",
-    adapter: node({
-      mode: "standalone"
-    }),
-    server: {
-      host: '0.0.0.0'
-    },
+export default defineConfig({
 })
